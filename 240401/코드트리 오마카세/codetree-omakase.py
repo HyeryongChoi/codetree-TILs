d265268bd08a4c3552e4a4e9749573af4e2b70ca
최혜령 <chex1004@gmail.com>
2024-04-01 16:41:05 +0900

    Title: 코드트리 오마카세, Time: 6550ms, Memory: 53MB, Status: Time Limit Exceed - CodeTree

--- 240401/코드트리 오마카세/codetree-omakase.py
+++ 240401/코드트리 오마카세/codetree-omakase.py
@@ -25,21 +25,26 @@ for c in raw_commands:
 # 손님 입장 200 t, x, name, n - name인 사람이 t시각 x위치에 n개의 초밥을 먹을 때까지 대기
 # 사진 촬영 300 t - t시각에 사람 수, 초밥 수 출력
 
-def pprint(board):
-    for row in board:
-        if len(row)> 0: print(*row)
+def pprint(dict_board):
+    for key, value in dict_board.items():
+        print(f'{key}: {value}')
     print()
 
 
 def rotate(belt):
-    last = belt.pop()
-    belt.appendleft(last)
+    new_belt = dict()
+    # belt = {key: value[:] for key, value in belt.items()}
+
+    for x in belt:
+        new_belt[(x+1)%L] = belt[x]
+    
+    return new_belt
 
 
 def eat(x, waited, belt):
     name, n = waited[x]
 
-    while n > 0 and name in belt[x]:
+    while n > 0 and x in belt and name in belt[x]:
         belt[x].remove(name)
         n-= 1
 
@@ -48,16 +53,18 @@ def eat(x, waited, belt):
 
 
 def eatAll(waited, belt):
-    for x in range(len(belt)):
-        if len(waited[x]) > 0:
+    for x in belt:
+        if x in waited and len(waited[x]) > 0:
             eat(x, waited, belt)
 
 
 
 
 def solution():
-    belt = deque([[] for _ in range(L)])
-    waited = [[] for _ in range(L)]
+    #belt = deque([[] for _ in range(L)])
+    #waited = [[] for _ in range(L)]
+    belt = dict()
+    waited = dict()
 
     # 각 초밥에 사람 이름을 적어서 회전하는 벨트 위에 올려 놓음
     # 원형 형태의 초밥 벨트, L개의 의자
@@ -68,10 +75,10 @@ def solution():
     prev_time = commands[0][1]
     for command in commands:
         op_code, time = command[0], command[1]
-
+ 
         # 초밥 벨트는 1초에 1칸씩 시계방향으로 회전
         for t in range(time-prev_time):
-            rotate(belt)
+            belt = rotate(belt)
             eatAll(waited, belt)
 
         # [1] 초밥 만들기
@@ -80,10 +87,12 @@ def solution():
 
             # t시각에 x 위치 앞에 name을 부착한 초밥 하나 올려놓음
             # 같은 위치에 여러 회전 초밥 가능, 같은 이름이 부착된 초밥 같은 위치 가능
+            if x not in belt: belt[x] = []
             belt[x].append(name)
+   
 
             # t시각에 x위치에서 초밥을 기다리는 손님이 있다면 초밥 먹기
-            if len(waited[x]) > 0:
+            if x in waited and len(waited[x]) > 0:
                 eat(x, waited, belt)
 
         
@@ -93,7 +102,7 @@ def solution():
 
             # x앞으로 오는 초밥들 중 자신의 이름이 적혀있는 초밥을 n개 먹고 자리를 뜸
             # 만약 t시각, x위치에 name초밥이 놓여 있다면 착석 즉시 먹게 되며, 동시에 여러 개 먹기 가능
-            while n > 0 and name in belt[x]:
+            while n > 0 and x in belt and name in belt[x]:
                 belt[x].remove(name)
                 n -= 1
             
@@ -110,11 +119,11 @@ def solution():
             p_count = 0
             c_count = 0
 
-            for w in waited:
-                if len(w) > 0: p_count += 1
+            for x in waited:
+                if len(waited[x]) > 0: p_count += 1
 
-            for b in belt:
-                c_count += len(b)
+            for x in belt:
+                c_count += len(belt[x])
             
             print(p_count, c_count)
         
